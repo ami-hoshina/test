99632ff940dc705000426f0a4a517e63a3b4390c
ほしなあみ <145949564+ami-hoshina@users.noreply.github.com>
2023-09-25 15:45:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('毎日を楽しく♪');
+const target = document.getElementById('target');
+//target.classList.add('red');
+target.addEventListener('click', () => {
+        target.classList.add('red');
+    });
